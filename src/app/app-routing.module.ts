560e25c1f0b86d29222c39a6a6c21561238bcb94
sabrina <issasabrina6@gmail.com>
2022-07-08 12:32:57 +0100

    new

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { OrdersComponent } from './orders/orders.component';
+import { AboutComponent } from './about/about.component';
+import { ServiceComponent } from './service/service.component';
+import { MakeorderComponent } from './makeorder/makeorder.component';
+import { PaymentComponent } from './payment/payment.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo: 'home',pathMatch: 'full'},
+  {path:'home',component:HomeComponent},
+  {path:'orders',component:OrdersComponent},
+  {path:'about',component:AboutComponent},
+  {path:'service',component:ServiceComponent},
+  {path:'makeorder',component:MakeorderComponent},
+  {path:'payment',component:PaymentComponent},
+  {path:'login',component:LoginComponent}
+  
+  
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,52 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { OrdersComponent } from './orders/orders.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatIconModule} from '@angular/material/icon';
+import { AboutComponent } from './about/about.component';
+import { ServiceComponent } from './service/service.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MakeorderComponent } from './makeorder/makeorder.component';
+import { PaymentComponent } from './payment/payment.component';
+import { LoginComponent } from './login/login.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    OrdersComponent,
+    AboutComponent,
+    ServiceComponent,
+    MakeorderComponent,
+    PaymentComponent,
+    LoginComponent,
+    
+   
+    
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatChipsModule,
+    MatIconModule,
+    MatFormFieldModule,
+
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
